bb464b92bfa2126f18c0cc341844202eea3cf9dd
Anurag Bajaj <anuragb26@gmail.com>
2019-08-13 18:42:52 +0530

    Feat:Add passport google outh

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const auth = require("./routes/auth");
+const passport = require("passport");
 const app = express();
 const port = process.env.PORT || 5000;
+require("./config/passport")(passport);
 
+app.use("/auth", auth);
 app.get("/", (req, res) => {
   res.send("It works");
 });
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://anuragb26:anuragb26@cluster0-nx9b3.mongodb.net/storybook?retryWrites=true&w=majority",
+  googleClientID:
+    "538861104207-92b8ld1pdsb2epsj9ggsijh2ggsr2mgl.apps.googleusercontent.com",
+  googleClientSecret: "vudYbLj08ezrxTLlp90qSFCn"
+};
